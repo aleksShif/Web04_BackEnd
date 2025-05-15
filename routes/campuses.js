1b8ec22c8e9df0636de10f4341b03d9c0e0e4f36
Sasha Shifrina <sasha.shifrina05@gmail.com>
2025-05-14 21:26:26 -0400

    frontend

--- routes/campuses.js
+++ routes/campuses.js
@@ -54,11 +54,11 @@ router.delete('/:id', ash(async(req, res) => {
   res.status(200).json("Deleted a campus!");
 }));
 
-/* ADD NEW CAMPUS */
-router.post('/', ash(async(req, res) => {
-  let newCampus = await Campus.create(req.body);
-  res.status(200).json(newCampus);  // Status code 200 OK - request succeeded
-}));
+// /* ADD NEW CAMPUS */
+// router.post('/', ash(async(req, res) => {
+//   let newCampus = await Campus.create(req.body);
+//   res.status(200).json(newCampus);  // Status code 200 OK - request succeeded
+// }));
 
 /* EDIT CAMPUS */
 router.put('/:id', ash(async(req, res) => {
